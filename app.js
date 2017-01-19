e2beac52f997c413e12a19ae0249ae989ff9c2de
Andrew Meier <ameier38@gmail.com>
2017-01-19 05:20:55 +0000

    updates

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 /* global $ */
 /* global Vue */
 
+var CLIENT_ID = 'NAXPCGUCT1C11XHKSBSMS3NC5OPYB1E0D2KJ3ACQE44SNH1Y'
+var CLIENT_SECRET = '3K2WYLQVRP0EF2YSOVDQPPVTURL53S55YBCWJFH05OMAC3T4'
+var LIMIT = 10
+var VERSION = '20161016'
+
 var store = {
     debug: true,
     state: {
@@ -8,6 +13,25 @@ var store = {
     },
     getRestaurants(address, foodType) {
         this.debug && console.log(`location: ${address}, foodType: ${foodType}`)
+        var host = 'https://api.foursquare.com'
+        var endpoint = '/v2/venues/search?'
+        var version_param = `v=${VERSION}`
+        var near_param = `near=${address}`
+        var query_param = `query=${foodType}`
+        var limit_param = `limit=${LIMIT}`
+        var client_id_param = `client_id=${CLIENT_ID}`
+        var client_secret_param = `client_secret=${CLIENT_SECRET}`
+        var params = [
+            version_param, near_param, query_param, limit_param,
+            client_id_param, client_secret_param
+        ].join('&')
+        var url = host + endpoint + params
+        this.debug && console.log(`url: ${url}`)
+        Vue.http.get(url).then((response) => {
+            this.state.restaurants = response.body.response.venues
+        }, (response) => {
+            console.log('Error!')
+        });
     }
 }
 
@@ -24,7 +48,7 @@ new Vue({
         }
     },
     methods: {
-        find: function(){
+        find: function() {
             store.getRestaurants(this.address, this.foodType)
         }
     }
